0adcb4898a51072f938e0f66d5e350e78a89959b
Zeus432 <msiddhu@outlook.com>
2022-12-01 23:11:18 +0530

    Day 1

---
+++ 2022/1.py
@@ -0,0 +1,16 @@
+with open("input.in") as fl:
+  calories = {i+1: sum([int(f) for f in x.split("\n")]) for i, x in  enumerate(fl.read().strip().split("\n\n"))}
+
+
+max_vals = sorted(calories, key=calories.get, reverse=True)[:3]
+
+print(max_vals)
+print(f"Fairy {max_vals[0]} is carrying the most calories - {calories[max_vals[0]]}")
+
+sum = 0
+print("Top 3 Fairies:")
+for i in max_vals:
+    print(f"Fairy {i} - {calories[i]}")
+    sum += calories[i]
+
+print(f"Total {sum} calories")
