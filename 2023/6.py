f8bebdfb8f1940a65df9b75c955e36f8021c66ab
Zeus432 <msiddharth879@gmail.com>
2023-12-06 12:09:41 +0530

    AOC 2023 - Day 6 (Part 2)

--- 2023/6.py
+++ 2023/6.py
@@ -3,13 +3,17 @@ with open("2023\\6.txt") as fl:
     time = list(map(int, time.split(":")[1].split()))
     distance = list(map(int, distance.split(":")[1].split()))
 
+    time.append(int("".join(map(str, time))))
+    distance.append(int("".join(map(str, distance))))
+
 net = 1
-for i in range(len(distance)):
+for i in range((length := len(distance))):
     win = 0
     for speed in range((t := time[i])):
         if speed * (t - speed) > distance[i]:
             win += 1
 
-    net *= 1 if win == 0 else win
+    net *= 1 if win == 0 or i == length - 1 else win
 
 print(f"Product of number of ways to beat record: {net}")
+print(f"Number of ways to beat final record: {win}")
