9e8663c8ee52061303c0b1eb64e988f8caa466bd
Zeus432 <msiddharth879@gmail.com>
2023-12-07 13:30:23 +0530

    AOC 2023 - Day 7 (Part 1)

---
+++ 2023/7.py
@@ -0,0 +1,32 @@
+with open("2023\\7.txt") as fl:
+    lines = [line.split() for line in fl.read().strip().split("\n")]
+
+card_map = {"T": "A", "J": "B", "Q": "C", "K": "D", "A": "E"}
+
+
+def counter(cards: str):
+    count = [list(cards).count(char) for char in cards]
+    if max(count) > 3:
+        return max(count) + 1
+    elif max(count) == 3 and 2 in count:
+        return 4
+    elif max(count) == 3:
+        return 3
+    elif count.count(2) == 4:
+        return 2
+    elif max(count) == 2:
+        return 1
+    return 0
+
+
+def preference(cards: str):
+    return (counter(cards), [card_map.get(card, card) for card in cards])
+
+
+lines.sort(key=lambda line: preference(line[0]))
+
+net = 0
+for rank, (cards, bid) in enumerate(lines, 1):
+    net += rank * int(bid)
+
+print(f"Total winnings: {net}")
