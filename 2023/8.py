17df0ea7ac0c999ec7089d7910083f652328d02b
Zeus432 <msiddharth879@gmail.com>
2023-12-08 11:15:15 +0530

    AOC 2023 - Day 8 (Part 2)

--- 2023/8.py
+++ 2023/8.py
@@ -1,3 +1,5 @@
+from math import lcm
+
 with open("2023\\8.txt") as fl:
     directions, sp, *nodes = fl.read().strip().split("\n")
 
@@ -14,5 +16,23 @@ while current_node != "ZZZ":
     for direc in directions:
         current_node = nodes[current_node][0 if direc == "L" else 1]
         steps += 1
+        if current_node == "ZZZ":
+            break
+
+
+steps_list = []
+for current_node in nodes.keys():
+    if current_node.endswith("A"):
+        step2 = 0
+        while current_node.endswith("Z") is not True:
+            for direc in directions:
+                current_node = nodes[current_node][0 if direc == "L" else 1]
+                step2 += 1
+                if current_node.endswith("Z") is True:
+                    break
+        if step2 != 0:
+            steps_list.append(step2)
+
 
 print(f"Number of steps taken: {steps}")
+print(f"Number of steps taken for all nodes: {lcm(*steps_list)}")
