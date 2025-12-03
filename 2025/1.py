25ce117c9638e7d7f1193210d99c0a8122c05a83
Siddharth M <msiddharth879@gmail.com>
2025-12-03 19:52:39 +0530

    day 2 and small changes to day 1

--- 2025/1.py
+++ 2025/1.py
@@ -1,10 +1,3 @@
-with open("2025\\1.txt") as fl:
-    lines = [[i[0], int(i[1:])] for i in fl.read().strip().split("\n")]
-
-num = 50
-counter = 0
-counter2 = 0
-
 def count_zeros(lines, start=50):
     num = start
     count1 = 0
@@ -28,6 +21,10 @@ def count_zeros(lines, start=50):
             count1 += 1
     
     return count1, count2
+
+
+with open("2025\\1.txt") as fl:
+    lines = [[i[0], int(i[1:])] for i in fl.read().strip().split("\n")]
 
 part1, part2 = count_zeros(lines, 50)
 print("Part 1:", part1)
---
+++ 2025/2.py
@@ -0,0 +1,30 @@
+def count_valid(lines):
+    invalid1 = 0
+    invalid2 = 0
+    
+    for start, end in lines:
+        for val in range(start, end+1):
+            obj = str(val)
+            cache = ""
+
+            for i in obj:
+                if cache.startswith(i) and cache * (count := round(len(obj)/len(cache))) == obj:
+
+                    if count % 2 == 0 and cache * count == obj:
+                        invalid1 += val
+
+                    invalid2 += val
+                    break
+
+                else:
+                    cache += i
+    
+    return invalid1, invalid2
+
+
+with open("2025\\2.txt") as fl:
+    lines = [[int(j) for j in i.split("-")] for i in fl.read().strip().split(",")]
+
+part1, part2 = count_valid(lines)
+print("Part 1:", part1)
+print("Part 2:", part2)
