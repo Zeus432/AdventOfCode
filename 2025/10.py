b7b1a8d61d5017ef1f50778eea914a9a3d883646
Siddharth M <msiddharth879@gmail.com>
2025-12-05 23:31:41 +0530

    prep for future days

---
+++ 2025/10.py
@@ -0,0 +1,31 @@
+import time
+
+
+class Day10:
+    def __init__(self):
+        self._start = time.perf_counter()
+        self.load_input()
+    
+    def load_input(self):
+        with open("2025\\10.txt") as fl:
+            self.lines = fl.read().strip().split("\n")
+    
+    def part_one(self):
+        return 0
+
+    def part_two(self):
+        return 0
+    
+    def solve(self, quiet=False):
+        if not quiet:
+            print("Part 1:", self.part_one())
+            print("Part 2:", self.part_two())
+            print(f"Runtime: {(time.perf_counter() - self._start) * 1000:.3f} ms")
+
+
+Day10().solve()
+
+"""
+import timeit
+print(f"{timeit.timeit("Day10().solve(quiet=True)", globals=globals(), number=1000):.3f} ms per run")
+"""  # Average runtime over 1000 runs
