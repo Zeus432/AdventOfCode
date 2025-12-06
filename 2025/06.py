ac67d859d0cd3ef04a4b91e87873712b68028cb7
Siddharth M <msiddharth879@gmail.com>
2025-12-07 00:04:06 +0530

    AOC 2025 - Day 6

--- 2025/06.py
+++ 2025/06.py
@@ -5,17 +5,70 @@ class Day6:
     def __init__(self):
         self._start = time.perf_counter()
         self.load_input()
-    
+
     def load_input(self):
         with open("2025\\06.txt") as fl:
-            self.lines = fl.read().strip().split("\n")
-    
+            lines = fl.read().strip().split("\n")
+            # print(lines)
+            start = -1
+            end = -1
+            nums = []
+            self.operators = lines[-1].split()
+
+            for index in range(z := len(lines[:-1][0])):  # part 2 cooked fr
+                gap = True
+                for line in lines[:-1]:
+                    if line[index] != " ":
+                        gap = False
+                        break
+
+                if gap is True:
+                    start = end + 1
+                    end = index
+                    nums.append([l[start:end] for l in lines[:-1]])
+
+                elif index + 1 == z:
+                    nums.append([l[end + 1 :] for l in lines[:-1]])
+
+            self.lines = nums
+
     def part_one(self):
-        return 0
+        lines = self.lines
+        operators = self.operators
+        total = 0
+
+        for index, ope in enumerate(operators):
+            cache = 0 if ope == "+" else 1
+
+            for val in lines[index]:
+                if ope == "+":
+                    cache += int(val.strip())
+                else:
+                    cache *= int(val.strip())
+
+            total += cache
+
+        return total
 
     def part_two(self):
-        return 0
-    
+        lines: list[str] = self.lines
+        operators = self.operators
+        total = 0
+
+        for index, ope in enumerate(operators):
+            cache = 0 if ope == "+" else 1
+            work = lines[index]
+
+            for place in range(-1, -(len(work[0]) + 1), -1):
+                if ope == "+":
+                    cache += int("".join([m[place] for m in work]))
+                else:
+                    cache *= int("".join([m[place] for m in work]))
+
+            total += cache
+
+        return total
+
     def solve(self, quiet=False):
         if not quiet:
             print("Part 1:", self.part_one())
