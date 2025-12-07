7c2b40ec44136f38b6e64b30c674771327db9860
Siddharth M <msiddharth879@gmail.com>
2025-12-07 14:15:57 +0530

    AOC 2025 - Day 7

--- 2025/07.py
+++ 2025/07.py
@@ -5,21 +5,44 @@ class Day7:
     def __init__(self):
         self._start = time.perf_counter()
         self.load_input()
-    
+
     def load_input(self):
         with open("2025\\07.txt") as fl:
-            self.lines = fl.read().strip().split("\n")
-    
-    def part_one(self):
-        return 0
+            self.lines = [list(a) for a in fl.read().strip().split("\n")]
 
-    def part_two(self):
-        return 0
-    
+    def part_one_and_two(self):
+        lines = self.lines
+        beams = {lines[0].index("S"): 1}
+        split = 0
+
+        for line in lines[1:]:
+            beam_copy = [(x, y) for x, y in beams.items()]
+            for pos, val in beam_copy:
+                if line[pos] == ".":
+                    line[pos] = "|" # just for visuals no impact if removed
+
+                elif line[pos] == "^":
+                    split += 1
+                    if (pos - 1) in beams:
+                        beams[pos - 1] += val
+                    else:
+                        beams[pos - 1] = val
+
+                    if (pos + 1) in beams:
+                        beams[pos + 1] += val
+                    else:
+                        beams[pos + 1] = val
+
+                    beams.pop(pos)
+
+        return split, sum(beams.values())
+
     def solve(self, quiet=False):
+        solution = self.part_one_and_two()
+
         if not quiet:
-            print("Part 1:", self.part_one())
-            print("Part 2:", self.part_two())
+            print("Part 1:", solution[0])
+            print("Part 2:", solution[1])
             print(f"Runtime: {(time.perf_counter() - self._start) * 1000:.3f} ms")
 
 
